b51b687ae0005abb90c1c72d114103edc96aa2db
Marcin Zdun <mzdun@midnightbits.com>
2013-12-04 14:40:32 +0100

    Start of working with a navigation history

--- navigator.cpp
+++ navigator.cpp
@@ -5,10 +5,44 @@ Navigator::Navigator(QObject *parent) :
 {
 }
 
+void Navigator::select(const HistoryItem& item)
+{
+    profiler::calls calls;
+    if (item.get_calls().empty())
+        calls = m_data->select<profiler::calls>();
+    else
+    {
+        for (profiler::call_id call: item.get_calls())
+        {
+            auto part = m_data->selectCalledFrom(call);
+            for (auto&& c: part)
+                calls.push_back(c);
+        }
+    }
+}
+
 void Navigator::back()
 {
+    if (m_history.empty())
+        return;
+
+    auto item = m_history.top();
+    m_history.pop();
+
+    if (m_history.empty())
+        hasHistory(false);
+
+    select(item);
 }
 
 void Navigator::home()
+{
+    History empty;
+    m_history.swap(empty);
+    hasHistory(false);
+    select(HistoryItem());
+}
+
+void Navigator::cancel()
 {
 }
--- navigator.h
+++ navigator.h
@@ -3,15 +3,32 @@
 
 #include <QObject>
 #include "profiler.h"
+#include <stack>
+
+class HistoryItem
+{
+public:
+    typedef std::vector<profiler::call_id> calls;
+
+    const calls& get_calls() const { return m_calls; }
+
+private:
+    calls m_calls;
+};
+
+typedef std::stack<HistoryItem> History;
 
 class Navigator : public QObject
 {
     Q_OBJECT
 
-    profiler::data* m_data;
+    profiler::data_ptr m_data;
+    History m_history;
+
+    void select(const HistoryItem& item);
 public:
     explicit Navigator(QObject *parent = 0);
-    void setData(profiler::data* data) { m_data = data; }
+    void setData(const profiler::data_ptr& data) { m_data = data; }
 
 signals:
     void hasHistory(bool);
@@ -19,6 +36,7 @@ signals:
 public slots:
     void back();
     void home();
+    void cancel();
 };
 
 #endif // NAVIGATOR_H
