3b744f23daa6a77baf1439956521ed1d16621583
Marcin Zdun <mzdun@midnightbits.com>
2013-12-13 07:04:18 +0100

    Setting proper small icon on Win7

--- viewer/win32_setIcon.cpp
+++ viewer/win32_setIcon.cpp
@@ -3,7 +3,12 @@
 
 extern "C" void win32_setIcon(HWND hwnd)
 {
-    HINSTANCE inst = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
-    HICON icon = LoadIcon(inst, MAKEINTRESOURCE(101));
-    SetClassLongPtr(hwnd, GCLP_HICON, (LONG_PTR)icon);
+	HINSTANCE inst = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
+	HICON icon = LoadIcon(inst, MAKEINTRESOURCE(101));
+	SetClassLongPtr(hwnd, GCLP_HICON, (LONG_PTR)icon);
+
+	icon = (HICON)LoadImage(inst, MAKEINTRESOURCE(101), IMAGE_ICON,
+		GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON),
+		LR_DEFAULTCOLOR);
+	SetClassLongPtr(hwnd, GCLP_HICONSM, (LONG_PTR)icon);
 }
